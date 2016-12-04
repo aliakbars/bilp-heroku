3bd3fdf730a543865df9bbde9e8fa66d22071069
Yosef Ardhito <yosefardhitowin@gmail.com>
2016-12-05 00:37:22 +0700

    can add new sentence and do check on words with kateglo API

--- main.py
+++ main.py
@@ -6,11 +6,48 @@ from flask.ext.heroku import Heroku
 from collections import Counter
 from datetime import datetime
 
+import requests
+import json
+from nltk.tokenize import TweetTokenizer
+
 app = Flask(__name__)
 heroku = Heroku(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///bilpdev"
 db = SQLAlchemy(app)
 app.debug = False
 app.secret_key = 'jbblhivghyrvcl_ec2-107-20-178-83'
+
+
+class Word(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
+	word = db.Column(db.String(40))
+	exist = db.Column(db.Boolean)
+	created_at = db.Column(db.DateTime)
+
+	def __init__(self, sentence_id, word, exist):
+		self.sentence_id = sentence_id
+		self.word = word
+		self.exist = exist
+		self.created_at = datetime.today()
+
+	def __repr__(self):
+		return '<Word %d - %s [%s]>' % (self.sentence_id, self.word, self.exist)
+
+class Sentence(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(255))
+	source = db.Column(db.String(40))
+	words = db.relationship('Word', backref='sentence', lazy='dynamic')
+	created_at = db.Column(db.DateTime)
+
+	def __init__(self, text, source):
+		self.text = text
+		self.source = source
+		self.created_at = datetime.today()
+
+	def __repr__(self):
+		return '<Sentence %s - %s>' % (self.source, self.text)
 
 class Dictionary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,6 +90,52 @@ def words():
 	c = Counter(((d.word_alay, d.word_normal) for d in dicts))
 	d = sorted(c.items())
 	return render_template('words.html', dicts=d)
+
+# comparator
+@app.route('/sentence', methods=['GET', 'POST'])
+def sentence():
+	errors = []
+
+	if request.method == 'POST':
+		if not request.form['sentence']:
+			errors.append('Isilah semua kolom yang tersedia!')
+		else:
+			d = Sentence(request.form['sentence'], request.form['source'])
+			db.session.add(d)
+			db.session.commit()
+			flash('Data berhasil tersimpan.')
+			return redirect('/sentence')
+	return render_template('sentence.html', errors=errors)
+
+@app.route('/check', methods=['GET'])
+def check():
+	check_id = request.args.get("id")
+	if check_id is not None:
+		check_sentence = Sentence.query.get(check_id)
+		if check_sentence is not None:
+			Word.query.filter_by(sentence_id=check_id).delete()
+			tweet_tokenizer = TweetTokenizer()
+			tokens = tweet_tokenizer.tokenize(check_sentence.text)
+			for token in tokens:
+				url = "http://kateglo.com/api.php?format=json&phrase="+token
+				resp = requests.get(url)
+				exist = False
+				if (resp.ok):
+					try:
+						resp_json = json.loads(resp.content)
+						exist = True
+					except ValueError:
+						exist = False
+				word = Word(check_sentence.id, token, exist)
+				db.session.add(word)
+			db.session.commit()
+	sentences = Sentence.query.all()
+	c = ((sentence.id, 
+		sentence.source, 
+		sentence.text, 
+		((w.word, w.exist,) for w in sentence.words.all()), 
+		) for sentence in sentences)
+	return render_template('check.html', rows=c)
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
