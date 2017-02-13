4cf2dd6178205c6982c6abf7922747c8aaf3216d
Ali Akbar <mail.septiandri@gmail.com>
2017-02-13 21:36:09 +0700

    Lots of TODOs, changed database schema

--- main.py
+++ main.py
@@ -2,43 +2,43 @@
 import os
 from flask import Flask, flash, current_app, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.heroku import Heroku
 from collections import Counter
 from datetime import datetime
+from sqlalchemy.sql import func
 
+import random
 import requests
 import json
+import re
 from nltk.tokenize import TweetTokenizer
 
 app = Flask(__name__)
-heroku = Heroku(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///bilpdev"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 db = SQLAlchemy(app)
-app.debug = False
+app.debug = True
 app.secret_key = 'jbblhivghyrvcl_ec2-107-20-178-83'
 
+# class Word(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
+# 	word = db.Column(db.String(40))
+# 	exist = db.Column(db.Boolean)
+# 	created_at = db.Column(db.DateTime)
 
-class Word(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
-	word = db.Column(db.String(40))
-	exist = db.Column(db.Boolean)
-	created_at = db.Column(db.DateTime)
+# 	def __init__(self, sentence_id, word, exist):
+# 		self.sentence_id = sentence_id
+# 		self.word = word
+# 		self.exist = exist
+# 		self.created_at = datetime.today()
 
-	def __init__(self, sentence_id, word, exist):
-		self.sentence_id = sentence_id
-		self.word = word
-		self.exist = exist
-		self.created_at = datetime.today()
-
-	def __repr__(self):
-		return '<Word %d - %s [%s]>' % (self.sentence_id, self.word, self.exist)
+# 	def __repr__(self):
+# 		return '<Word %d - %s [%s]>' % (self.sentence_id, self.word, self.exist)
 
 class Sentence(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String(255))
 	source = db.Column(db.String(40))
-	words = db.relationship('Word', backref='sentence', lazy='dynamic')
+	# words = db.relationship('Word', backref='sentence', lazy='dynamic')
 	created_at = db.Column(db.DateTime)
 
 	def __init__(self, text, source):
@@ -53,32 +53,80 @@ class Dictionary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word_alay = db.Column(db.String(255))
     word_normal = db.Column(db.String(255))
+    sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
     user_agent = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime)
 
-    def __init__(self, word_alay, word_normal, user_agent):
+    def __init__(self, word_alay, word_normal, sentence_id, user_agent):
         self.word_alay = word_alay
         self.word_normal = word_normal
+        self.sentence_id = sentence_id
         self.user_agent = user_agent
         self.created_at = datetime.today()
 
     def __repr__(self):
-        return '<Content %s - %s>' % (self.word_alay, self.word_normal)
+        return '<Content %s - %s>' % (self.kata_id, self.word_normal)
+
+class User(db.Model):
+	# TODO Mungkin diganti dengan flask-login
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(40))
+	password = db.Column(db.String(255)) # hash
+	score = db.Column(db.Integer)
+	created_at = db.Column(db.DateTime)
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		self.score = 0
+		self.created_at = datetime.today()
+
+class ScoreLog(db.Model):
+	# TODO Add backref relationship
+	id = db.Column(db.Integer, primary_key=True)
+	dictionary_id = db.Column(db.Integer, db.ForeignKey('dictionary.id'))
+	target_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	score = db.Column(db.Integer)
+	created_at = db.Column(db.DateTime)
+
+	def __init__(self, dictionary_id, target_user_id, score):
+		self.target_user_id = target_user_id
+		self.score = score
+		self.created_at = datetime.today()
+
+def filter_words(words):
+	with open('kbbi.txt') as f:
+		lemmas = f.read().splitlines()
+		lemmas = set(lemmas)
+		return filter(lambda x: x not in lemmas, set(words))
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
 	errors = []
+	sentence = Sentence.query.order_by(func.random()).first()
+	words = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", sentence.text.lower()).split()
+	words = filter_words(words)
+	random.shuffle(words)
+	words = words[:4]
+
+	# TODO Memunculkan kata yang sebelumnya jika tidak berhasil
 
 	if request.method == 'POST':
-		if not request.form['word_alay'] or not request.form['word_normal']:
+		if not request.form.getlist('word_alay') or not request.form.getlist('word_normal') or not request.form['sentence_id']:
 			errors.append('Isilah semua kolom yang tersedia!')
 		else:
-			d = Dictionary(request.form['word_alay'], request.form['word_normal'], request.headers.get('User-Agent'))
-			db.session.add(d)
-			db.session.commit()
+			words_alay = request.form.getlist('word_alay')
+			words_normal = request.form.getlist('word_normal')
+			for i, word_alay in enumerate(words_alay):
+				word_normal = words_normal[i]
+				d = Dictionary(word_alay, word_normal, request.form['sentence_id'], request.headers.get('User-Agent'))
+				db.session.add(d)
+				db.session.commit()
 			flash('Data berhasil tersimpan.')
 			return redirect('/')
-	return render_template('index.html', errors=errors)
+
+	return render_template('index.html', errors=errors, sentence=sentence, words=words)
 
 @app.route('/success', methods=['GET'])
 def success():
@@ -113,6 +161,7 @@ def check():
 	if check_id is not None:
 		check_sentence = Sentence.query.get(check_id)
 		if check_sentence is not None:
+			# TODO Ganti dengan pengecekan menggunakan hasil scraping KBBI
 			Word.query.filter_by(sentence_id=check_id).delete()
 			tweet_tokenizer = TweetTokenizer()
 			tokens = tweet_tokenizer.tokenize(check_sentence.text)
@@ -136,6 +185,30 @@ def check():
 		((w.word, w.exist,) for w in sentence.words.all()), 
 		) for sentence in sentences)
 	return render_template('check.html', rows=c)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	raise NotImplementedError("")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	errors = []
+
+	if request.method == 'POST':
+		if not request.form['username'] and not request.form['password']:
+			errors.append('Isilah semua kolom!')
+		else:
+			# TODO Ganti dengan Flask-Login atau buat session
+			raise NotImplementedError("")
+	return render_template('signin.html', errors=errors)
+
+@app.route('/scores', methods=['GET'])
+def show_scores():
+	raise NotImplementedError("")
+
+def add_score():
+	# TODO Tambahkan score ke log dan update di tabel User
+	raise NotImplementedError("")
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
